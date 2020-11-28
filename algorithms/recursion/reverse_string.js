0823e2bd584f7c11f0e613ffab1f33acd4f16174
Dylan Buchi <devybuchi@gmail.com>
2020-11-27 21:22:47 -0300

    add reverse string iterative and one line solution

--- algorithms/recursion/reverse_string.js
+++ algorithms/recursion/reverse_string.js
@@ -1,8 +1,26 @@
-function reverseStringRecursive(str) {
+const reverseStringRecursive = (str) => {
     if (!str.length) {
         return "";
     }
     return reverseStringRecursive(str.substr(1)) + str[0];
-}
+};
+
+const reverseStringIterative = (str) => {
+    const parts = str.split("");
+    const array = [];
+
+    for (let i = parts.length - 1; i >= 0; i--) {
+        const ch = parts[i];
+        array.push(ch);
+    }
+
+    return array.join("");
+};
+
+const reverseStringOneLine = (str) => {
+    return str.split("").reverse().join("");
+};
 
 console.log(reverseStringRecursive("hello"));
+console.log(reverseStringIterative("hello"));
+console.log(reverseStringOneLine("hello"));
